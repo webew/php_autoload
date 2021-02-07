ec9759752a9047babaeebbaf101b4c0af5b9a6cf
webew <fillion.bertrand@gmail.com>
2021-02-07 21:55:29 +0100

    ajout autoload simple

---
+++ php_autoload/index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+require_once "autoload.php";
+
+use classes\package1\Main as Main1;
+use classes\package2\Main as Main2;
+
+$main1 = new Main1("ma propriété");
+echo $main1->methode();
+
+echo "<hr>";
+
+$main2 = new Main2("ma propriété");
+echo $main2->methode();
